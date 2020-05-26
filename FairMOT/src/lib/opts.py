361c5890853ef12b752fc0a319667952742f9d12
nadinenijssen <63190261+nadinenijssen@users.noreply.github.com>
2020-05-26 14:00:11 +0200

    Update opts.py split training/validation

--- FairMOT/src/lib/opts.py
+++ FairMOT/src/lib/opts.py
@@ -105,6 +105,8 @@ class opts(object):
     self.parser.add_argument('--val_mot16', default=False, help='val mot16 or mot15')
     self.parser.add_argument('--test_mot17', default=False, help='test mot17')
     self.parser.add_argument('--val_mot17', default=False, help='val mot17')
+    self.parser.add_argument('--training_mot17', default=False, help='training mot17')
+    self.parser.add_argument('--validation_mot17', default=False, help='validation mot17')
     self.parser.add_argument('--val_mot20', default=False, help='val mot20')
     self.parser.add_argument('--test_mot20', default=False, help='test mot20')
     self.parser.add_argument('--conf_thres', type=float, default=0.6, help='confidence thresh for tracking')
